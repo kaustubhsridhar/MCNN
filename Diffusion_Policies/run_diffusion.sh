cb02f918c7f59379a013317c31d59a78567646c9
kaustubhsridhar <kaysri2007@gmail.com>
2023-09-28 03:17:46 -0400

    minor

--- Diffusion_Policies/run_diffusion.sh
+++ Diffusion_Policies/run_diffusion.sh
@@ -2,19 +2,19 @@
 mkdir logs
 
 GPU=0
-
+SEED=0
 
 for TASKS in 'pen-human-v1 hammer-human-v1' 'relocate-human-v1 door-human-v1'
 do
     for task in ${TASKS}
     do
-        # CUDA_VISIBLE_DEVICES=${GPU} nohup python -u main.py --algo bc --env_name pen-human-v1 --device 0 --ms online --lr_decay > logs/bc_${task}.log &
+        CUDA_VISIBLE_DEVICES=${GPU} nohup python -u main.py --seed ${SEED} --algo bc --env_name pen-human-v1 --device 0 --ms online --lr_decay > logs/bc_${task}.log &
         
         for F in 0.1 0.025 0.05
         do
             for lamda in 1.0 #0.1 10.0
             do
-                CUDA_VISIBLE_DEVICES=${GPU} nohup python -u main.py --algo mcnn_bc --env_name ${task} --device 0 --ms online --lr_decay --num_memories_frac ${F} --Lipz 1.0 --lamda ${lamda} > logs/mcnn_bc_${task}_F${F}_lamda${lamda}.log &
+                CUDA_VISIBLE_DEVICES=${GPU} nohup python -u main.py --seed ${SEED} --algo mcnn_bc --env_name ${task} --device 0 --ms online --lr_decay --num_memories_frac ${F} --Lipz 1.0 --lamda ${lamda} > logs/mcnn_bc_${task}_F${F}_lamda${lamda}.log &
             done
         done
     done
